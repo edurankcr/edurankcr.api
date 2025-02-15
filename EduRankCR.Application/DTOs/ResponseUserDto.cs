bb354d0fe528c6cd480b1a1413507ab0e93a3bef
Alejandro Marín González <amaringonz.dev@gmail.com>
2025-02-15 17:11:33 -0600

    Add Request and Response DTOs for user operations and rename UserCreateDto

--- EduRankCR.Application/DTOs/ResponseUserDto.cs
+++ EduRankCR.Application/DTOs/ResponseUserDto.cs
@@ -2,21 +2,26 @@
 
 namespace EduRankCR.Application.DTOs
 {
-    public class UserCreateDto
+    public class ResponseUserDto
     {
         // Required properties
+        public required Guid Id { get; set; } = Guid.NewGuid();
         public required string Name { get; set; } = string.Empty;
         public required string LastName { get; set; } = string.Empty;
         public required string Username { get; set; } = string.Empty;
         public required string Email { get; set; } = string.Empty;
         public required bool EmailConfirmed { get; set; } = false;
         public required DateTime Birthdate { get; set; } = DateTime.MinValue;
-        public required string Password { get; set; } = string.Empty;
         public required UserRole Role { get; set; } = UserRole.User;
         public required UserStatus Status { get; set; } = UserStatus.Active;
         
         // Optional properties
+        public string? NewEmail { get; set; }
         public string? AvatarUrl { get; set; }
         public string? Biography { get; set; }
+        
+        // Additional properties
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
---
+++ EduRankCR.Application/DTOs/ResponseUserIdDto.cs
@@ -0,0 +1,7 @@
+﻿namespace EduRankCR.Application.DTOs
+{
+    public class ResponseUserIdDto
+    {
+        public required Guid Id { get; set; }
+    }
+}
