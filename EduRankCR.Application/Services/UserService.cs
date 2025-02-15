3d5e38bb1b027caa452e448975c0a76d9086e0d9
Alejandro Marín González <amaringonz.dev@gmail.com>
2025-02-15 17:13:59 -0600

    Refactor UserController, UserRepository, and UserService to use request and response DTOs, adding validation for user creation, retrieval, update, and deletion operations

--- EduRankCR.Application/Services/UserService.cs
+++ EduRankCR.Application/Services/UserService.cs
@@ -16,35 +16,35 @@ namespace EduRankCR.Application.Services
             _mapper = mapper;
         }
         
-        public async Task<UserDto> CreateUserAsync(UserCreateDto userCreateDto)
+        public async Task<ResponseUserDto> CreateUserAsync(RequestUserCreateDto requestUserCreateDto)
         {
-            userCreateDto.Password = BCrypt.Net.BCrypt.HashPassword(userCreateDto.Password);
-            User createdUser = await _userRepository.CreateUserAsync(userCreateDto);
-            return _mapper.Map<UserDto>(createdUser);
+            requestUserCreateDto.Password = BCrypt.Net.BCrypt.HashPassword(requestUserCreateDto.Password);
+            User createdUser = await _userRepository.CreateUserAsync(requestUserCreateDto);
+            return _mapper.Map<ResponseUserDto>(createdUser);
         }
         
-        public async Task<List<UserDto>> GetAllUsersAsync()
+        public async Task<List<ResponseUserDto>> GetAllUsersAsync()
         {
             List<User> users = await _userRepository.GetAllUsersAsync();
-            return _mapper.Map<List<UserDto>>(users);
+            return _mapper.Map<List<ResponseUserDto>>(users);
         }
         
-        public async Task<UserDto> GetUserByIdAsync(Guid id)
+        public async Task<ResponseUserDto> GetUserByIdAsync(Guid id)
         {
             User user = await _userRepository.GetUserByIdAsync(id);
-            return _mapper.Map<UserDto>(user);
+            return _mapper.Map<ResponseUserDto>(user);
         }
         
-        public async Task<UserDto> UpdateUserAsync(Guid id, UserUpdateDto userUpdateDto)
+        public async Task<ResponseUserDto> UpdateUserAsync(Guid id, RequestUserUpdateDto requestUserUpdateDto)
         {
-            User user = await _userRepository.UpdateUserAsync(id, userUpdateDto);
-            return _mapper.Map<UserDto>(user);
+            User user = await _userRepository.UpdateUserAsync(id, requestUserUpdateDto);
+            return _mapper.Map<ResponseUserDto>(user);
         }
         
-        public async Task<UserDto> DeleteUserAsync(Guid id)
+        public async Task<ResponseUserIdDto> DeleteUserAsync(Guid id)
         {
             User deletedUser = await _userRepository.DeleteUserAsync(id);
-            return _mapper.Map<UserDto>(deletedUser);
+            return _mapper.Map<ResponseUserIdDto>(deletedUser);
         }
     }
 }
