0331a228a1cdd2bc24ddceb79bf14960c00abf8a
Alejandro Marín González <amaringonz.dev@gmail.com>
2025-02-14 21:43:05 -0600

    Add UserUpdateDto for user update operations with required and optional properties

---
+++ EduRankCR.Application/DTOs/UserUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using EduRankCR.Domain.Enums;
+
+namespace EduRankCR.Application.DTOs
+{
+    public class UserUpdateDto
+    {
+        // Required properties
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public bool? EmailConfirmed { get; set; }
+        public DateTime? Birthdate { get; set; } = null;
+        public UserRole? Role { get; set; }
+        public UserStatus? Status { get; set; }
+        
+        // Optional properties
+        public string? AvatarUrl { get; set; }
+        public string? Biography { get; set; }
+    }
+}
