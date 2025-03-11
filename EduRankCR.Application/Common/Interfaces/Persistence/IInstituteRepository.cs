19ee76c2bf0a6b711d862e5b650b08d77ad88e1d
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:09:33 -0600

    feat: add Find method to IInstituteRepository for retrieving institutes by ID

--- EduRankCR.Application/Common/Interfaces/Persistence/IInstituteRepository.cs
+++ EduRankCR.Application/Common/Interfaces/Persistence/IInstituteRepository.cs
@@ -1,8 +1,10 @@
 ﻿using EduRankCR.Domain.InstituteAggregate.Entities;
+using EduRankCR.Domain.InstituteAggregate.ValueObjects;
 
 namespace EduRankCR.Application.Common.Interfaces.Persistence;
 
 public interface IInstituteRepository
 {
     Task Create(Institute institute);
+    Task<Institute?> Find(InstituteId instituteId);
 }
