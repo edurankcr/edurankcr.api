ac8fbed85ecaec12cfc45681168e53d130a5bd04
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-10 21:58:10 -0600

    feat: add stored procedure for creating a new teacher

---
+++ EduRankCR.Infrastructure/Migrations/Teacher/sp_Teacher__Create.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE sp_Teacher__Create
+    @UserId UNIQUEIDENTIFIER,
+    @InstituteId UNIQUEIDENTIFIER = NULL,
+    @Name NVARCHAR(100),
+    @LastName NVARCHAR(100),
+    @Status TINYINT = 0
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Insert new teacher record
+    INSERT INTO Teachers (TeacherId, UserId, InstituteId, Name, LastName, Status, CreatedAt, UpdatedAt)
+    VALUES (NEWID(), @UserId, @InstituteId, @Name, @LastName, @Status, GETDATE(), GETDATE());
+END
