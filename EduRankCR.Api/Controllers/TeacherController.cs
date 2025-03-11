b2a38f1922820a2226455467c1e9e80783094407
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:10:07 -0600

    feat: add TeacherController for handling teacher creation requests

---
+++ EduRankCR.Api/Controllers/TeacherController.cs
@@ -0,0 +1,43 @@
+﻿using EduRankCR.Application.Commands.Teacher.Commands.Create;
+using EduRankCR.Contracts.Common;
+using EduRankCR.Contracts.Teacher;
+using EduRankCR.Domain.Common.Errors;
+using MapsterMapper;
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduRankCR.Api.Controllers;
+
+[Route("teacher")]
+public class TeacherController : ApiController
+{
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public TeacherController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateTeacherRequest request)
+    {
+        var userId = GetUserId();
+
+        if (userId is null)
+        {
+            return Problem(
+                statusCode: StatusCodes.Status401Unauthorized,
+                title: Errors.Auth.Unauthorized.Description);
+        }
+
+        var command = _mapper.Map<CreateTeacherCommand>(request) with { UserId = userId };
+        var response = await _mediator.Send(command);
+
+        return response.Match(
+            result => Ok(_mapper.Map<BoolResponse>(result)),
+            Problem);
+    }
+}
