902926ec02a7068c1ae54b269b417830db05273e
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:10:16 -0600

    feat: add TeacherRepository for managing teacher data persistence

---
+++ EduRankCR.Infrastructure/Persistence/Repositories/TeacherRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Dapper;
+using EduRankCR.Application.Common.Interfaces.Persistence;
+using EduRankCR.Domain.TeacherAggregate.Entities;
+
+namespace EduRankCR.Infrastructure.Persistence.Repositories;
+
+public class TeacherRepository : ITeacherRepository
+{
+    private readonly IDbContext _connectionFactory;
+
+    public TeacherRepository(IDbContext connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task Create(Teacher teacher)
+    {
+        using IDbConnection connection = _connectionFactory.CreateConnection();
+
+        var parameters = new DynamicParameters();
+        parameters.Add("@UserId", teacher.UserId.Value);
+        parameters.Add("@InstituteId", teacher.InstituteId.Value);
+        parameters.Add("@Name", teacher.Name);
+        parameters.Add("@LastName", teacher.LastName);
+        parameters.Add("@Status", teacher.Status);
+
+        await connection.QueryAsync("sp_Teacher__Create", parameters, commandType: CommandType.StoredProcedure);
+    }
+}
