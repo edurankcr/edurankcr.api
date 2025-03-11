2c861d13082a76944c0a96c1b7215d597fcca405
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:09:08 -0600

    feat: add CreateTeacherCommandValidator for input validation

---
+++ EduRankCR.Application/Commands/Teacher/Commands/Create/CreateTeacherCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace EduRankCR.Application.Commands.Teacher.Commands.Create;
+
+public class CreateTeacherCommandValidator : AbstractValidator<CreateTeacherCommand>
+{
+    public CreateTeacherCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.InstituteId).NotNull();
+        RuleFor(x => x.UserId).NotNull();
+    }
+}
