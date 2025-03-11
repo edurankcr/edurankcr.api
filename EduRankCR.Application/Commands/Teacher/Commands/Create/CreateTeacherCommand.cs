09bc8755fb689eb1da7668a5c90ef369265fe7f3
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:08:49 -0600

    feat: implement CreateTeacher command and associated repository methods

---
+++ EduRankCR.Application/Commands/Teacher/Commands/Create/CreateTeacherCommand.cs
@@ -0,0 +1,13 @@
+﻿using EduRankCR.Application.Common;
+
+using ErrorOr;
+
+using MediatR;
+
+namespace EduRankCR.Application.Commands.Teacher.Commands.Create;
+
+public record CreateTeacherCommand(
+    string Name,
+    string LastName,
+    string InstituteId,
+    string UserId) : IRequest<ErrorOr<BoolResult>>;
