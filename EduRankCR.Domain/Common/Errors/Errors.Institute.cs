ef47915f800c62982b07c52a7baaa1e1beb7e2ae
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-11 11:09:29 -0600

    feat: add NotFound error for Institute in error handling

---
+++ EduRankCR.Domain/Common/Errors/Errors.Institute.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+
+namespace EduRankCR.Domain.Common.Errors;
+
+public static partial class Errors
+{
+    public static class Institute
+    {
+        public static Error NotFound =>
+            Error.NotFound(code: "Institute.NotFound", description: "Institute not found in the system.");
+    }
+}
