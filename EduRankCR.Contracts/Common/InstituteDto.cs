a8ae5cff781acaec572639aa9f6cfae3f1c7ab5b
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 22:54:17 -0600

    feat: add InstituteDto record for institute data representation

---
+++ EduRankCR.Contracts/Common/InstituteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EduRankCR.Contracts.Common;
+
+public record InstituteDto(
+    Guid InstituteId,
+    string Name,
+    byte Type,
+    byte Province,
+    short District,
+    string? Url);
