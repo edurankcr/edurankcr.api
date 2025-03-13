a364cbf92a85476d538da04fb49a2d5c3236521a
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 22:53:52 -0600

    feat: update SearchResponse to use TeacherDto and InstituteDto for data representation

--- EduRankCR.Contracts/Search/SearchResponse.cs
+++ EduRankCR.Contracts/Search/SearchResponse.cs
@@ -1,20 +1,7 @@
-﻿namespace EduRankCR.Contracts.Search;
+﻿using EduRankCR.Contracts.Common;
+
+namespace EduRankCR.Contracts.Search;
 
 public record SearchResponse(
-    List<TeacherSummaryDto> Teachers,
-    List<InstituteSummaryDto> Institutes);
-
-public record TeacherSummaryDto(
-    Guid TeacherId,
-    string Name,
-    string LastName,
-    Guid InstituteId,
-    string InstituteName);
-
-public record InstituteSummaryDto(
-    Guid InstituteId,
-    string Name,
-    int Type,
-    int Province,
-    int District,
-    string? Url);
+    List<TeacherDto> Teachers,
+    List<InstituteDto> Institutes);
