7b67bf689f3f1d975458a0ed9b0b22a3970af103
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 21:26:22 -0600

    feat: add SearchInstituteQueryHandler for processing institute search queries

---
+++ EduRankCR.Application/Commands/Institute/Query/Search/SearchInstituteQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using EduRankCR.Application.Commands.Institute.Common;
+using EduRankCR.Application.Common.Interfaces.Persistence;
+using EduRankCR.Domain.Common.Errors;
+using EduRankCR.Domain.InstituteAggregate.ValueObjects;
+
+using ErrorOr;
+
+using MediatR;
+
+namespace EduRankCR.Application.Commands.Institute.Query.Search;
+
+public class SearchInstituteQueryHandler : IRequestHandler<SearchInstituteQuery, ErrorOr<SearchInstituteResult>>
+{
+    private readonly IInstituteRepository _instituteRepository;
+
+    public SearchInstituteQueryHandler(IInstituteRepository instituteRepository)
+    {
+        _instituteRepository = instituteRepository;
+    }
+
+    public async Task<ErrorOr<SearchInstituteResult>> Handle(
+        SearchInstituteQuery query,
+        CancellationToken cancellationToken)
+    {
+        Domain.InstituteAggregate.Entities.Institute? institute = await _instituteRepository.Find(InstituteId.ConvertFromString(query.InstituteId));
+
+        if (institute?.Id is null)
+        {
+            return Errors.Institute.NotFound;
+        }
+
+        return new SearchInstituteResult(institute);
+    }
+}
