8d48bda29ed2a590996d4a60e93b9c5aaea30108
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 21:26:48 -0600

    feat: add SearchQuery for querying search parameters

---
+++ EduRankCR.Application/Commands/Search/Queries/SearchQuery.cs
@@ -0,0 +1,15 @@
+﻿using EduRankCR.Application.Commands.Search.Common;
+
+using ErrorOr;
+
+using MediatR;
+
+namespace EduRankCR.Application.Commands.Search.Queries;
+
+public record SearchQuery(
+    string Name,
+    string? Type, // teacher, institution or all
+    string? InstituteId,
+    int? TypeFilter, // institution type
+    int? Province,
+    int? District) : IRequest<ErrorOr<SearchResult>>;
