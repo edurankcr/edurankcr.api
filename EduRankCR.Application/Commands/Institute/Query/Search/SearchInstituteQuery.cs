3019857a354adbd8ad446c83795701b67ac843a2
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 21:26:11 -0600

    feat: add SearchInstituteQuery for querying institute details

---
+++ EduRankCR.Application/Commands/Institute/Query/Search/SearchInstituteQuery.cs
@@ -0,0 +1,9 @@
+﻿using EduRankCR.Application.Commands.Institute.Common;
+
+using ErrorOr;
+
+using MediatR;
+
+namespace EduRankCR.Application.Commands.Institute.Query.Search;
+
+public record SearchInstituteQuery(string InstituteId) : IRequest<ErrorOr<SearchInstituteResult>>;
