4a9055e83902d5655e7027ca9cf07cac2ee10728
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 21:25:53 -0600

    feat: add InstituteSummaryDto for summarizing institute data

---
+++ EduRankCR.Application/DTOs/InstituteSummaryDto.cs
@@ -0,0 +1,23 @@
+﻿namespace EduRankCR.Application.DTOs;
+
+public class InstituteSummaryDto
+{
+    public Guid InstituteId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Type { get; set; }
+    public int Province { get; set; }
+    public int District { get; set; }
+    public string? Url { get; set; }
+
+    public InstituteSummaryDto() { }
+
+    public InstituteSummaryDto(Guid instituteId, string name, int type, int province, int district, string? url)
+    {
+        InstituteId = instituteId;
+        Name = name;
+        Type = type;
+        Province = province;
+        District = district;
+        Url = url;
+    }
+}
