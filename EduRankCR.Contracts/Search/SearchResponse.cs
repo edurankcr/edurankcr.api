c1226c47a7b7bc07eac5230e2c7b71014129b3c0
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-12 21:27:18 -0600

    feat: add SearchResponse record for encapsulating search results

---
+++ EduRankCR.Contracts/Search/SearchResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace EduRankCR.Contracts.Search;
+
+public record SearchResponse(
+    List<TeacherSummaryDto> Teachers,
+    List<InstituteSummaryDto> Institutes);
+
+public record TeacherSummaryDto(
+    Guid TeacherId,
+    string Name,
+    string LastName,
+    Guid InstituteId,
+    string InstituteName);
+
+public record InstituteSummaryDto(
+    Guid InstituteId,
+    string Name,
+    int Type,
+    int Province,
+    int District,
+    string? Url);
