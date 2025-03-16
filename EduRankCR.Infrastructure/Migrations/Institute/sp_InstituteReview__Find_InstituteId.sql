ed0529df14f15327637e8b364ab46be5d5edde1c
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 12:38:20 -0600

    feat: add stored procedure for finding institute reviews by user and institute

---
+++ EduRankCR.Infrastructure/Migrations/Institute/sp_InstituteReview__Find_InstituteId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE sp_InstituteReview__Find_InstituteId
+    @UserId UNIQUEIDENTIFIER,
+    @InstituteId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT ReviewId, UserId, InstituteId, Reputation, Opportunities, Happiness, Location, Facilities, Social, Clubs, Internet,
+           Security, Food, ExperienceText, Status, CreatedAt, UpdatedAt
+    FROM InstitutesReviews
+    WHERE UserId = @UserId AND InstituteId = @InstituteId;
+END;
