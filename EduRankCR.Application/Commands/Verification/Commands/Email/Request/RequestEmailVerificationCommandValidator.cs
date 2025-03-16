53a987dfa1803e9933a1631c956a2cd3a7480cee
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 13:51:05 -0600

    feat: enhance RequestEmailVerificationCommandValidator to validate email format and provider

--- EduRankCR.Application/Commands/Verification/Commands/Email/Request/RequestEmailVerificationCommandValidator.cs
+++ EduRankCR.Application/Commands/Verification/Commands/Email/Request/RequestEmailVerificationCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using EduRankCR.Application.Common.Utils;
+
+using FluentValidation;
 
 namespace EduRankCR.Application.Commands.Verification.Commands.Email.Request;
 
@@ -6,6 +8,15 @@ public class RequestEmailVerificationCommandValidator : AbstractValidator<Reques
 {
     public RequestEmailVerificationCommandValidator()
     {
-        RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(256);
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .MaximumLength(256)
+            .Must(BeValidEmail)
+            .WithMessage("Email must be from a common provider (e.g., Gmail, Yahoo, Hotmail, iCloud).");
+    }
+
+    private bool BeValidEmail(string email)
+    {
+        return ValidationHelper.IsValidEmail(email);
     }
 }
