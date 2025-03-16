acae4ab8f7f588ceafef67c2b82385e8ec6aa0ab
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-15 23:13:21 -0600

    feat: add stored procedure for finding a teacher by ID

---
+++ EduRankCR.Infrastructure/Migrations/Teacher/sp_Teacher__Find_Id.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE sp_Teacher__Find_Id
+    @TeacherId UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT TeacherId, UserId, InstituteId, Name, LastName, Status, CreatedAt, UpdatedAt
+    FROM Teachers
+    WHERE TeacherId = @TeacherId;
+END;
