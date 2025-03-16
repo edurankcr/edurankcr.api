35cb5ccfb91d309a63e447983309863dcf8ba656
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-15 23:13:15 -0600

    feat: add stored procedure for creating teacher reviews

---
+++ EduRankCR.Infrastructure/Migrations/Teacher/sp_TeacherReview__Create.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE sp_TeacherReview__Create
+(
+    @UserId UNIQUEIDENTIFIER,
+    @TeacherId UNIQUEIDENTIFIER,
+    @FreeCourse BIT,
+    @CourseCode NVARCHAR(64) = NULL,
+    @CourseMode TINYINT,
+    @ProfessorRating DECIMAL(2,1),
+    @DifficultyRating DECIMAL(2,1),
+    @WouldTakeAgain BIT = NULL,
+    @MandatoryAttendance BIT = NULL,
+    @GradeReceived NVARCHAR(5) = NULL,
+    @ExperienceText NVARCHAR(510),
+    @Status TINYINT = 0,
+    @CreatedAt DATETIME2 = NULL,
+    @UpdatedAt DATETIME2 = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF @CreatedAt IS NULL SET @CreatedAt = GETDATE();
+    IF @UpdatedAt IS NULL SET @UpdatedAt = GETDATE();
+
+    INSERT INTO TeachersReviews (
+        ReviewId, UserId, TeacherId, FreeCourse, CourseCode, CourseMode,
+        ProfessorRating, DifficultyRating, WouldTakeAgain, MandatoryAttendance,
+        GradeReceived, ExperienceText, Status, CreatedAt, UpdatedAt
+    )
+    VALUES (
+               NEWID(), @UserId, @TeacherId, @FreeCourse, @CourseCode, @CourseMode,
+               @ProfessorRating, @DifficultyRating, @WouldTakeAgain, @MandatoryAttendance,
+               @GradeReceived, @ExperienceText, @Status, @CreatedAt, @UpdatedAt
+           );
+END;
