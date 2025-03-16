0d4560ad2a901d325c29bb272da065c356eb6b07
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 13:50:54 -0600

    feat: add ProfileQueryValidator to validate UserId format and presence

---
+++ EduRankCR.Application/Commands/Profile/Queries/Profile/ProfileQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace EduRankCR.Application.Commands.Profile.Queries.Profile;
+
+public class ProfileQueryValidator : AbstractValidator<ProfileQuery>
+{
+    public ProfileQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required.")
+            .Must(id => Guid.TryParse(id, out _)).WithMessage("Invalid UserId format.");
+    }
+}
