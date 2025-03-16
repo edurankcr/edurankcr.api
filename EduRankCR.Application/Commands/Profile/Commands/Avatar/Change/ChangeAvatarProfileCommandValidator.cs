6dc64d2763db4dafb876af5d3e168c1ecfc37a58
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 13:50:03 -0600

    feat: enhance ChangeAvatarProfileCommandValidator to require UserId and improve avatar validation

--- EduRankCR.Application/Commands/Profile/Commands/Avatar/Change/ChangeAvatarProfileCommandValidator.cs
+++ EduRankCR.Application/Commands/Profile/Commands/Avatar/Change/ChangeAvatarProfileCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using EduRankCR.Application.Common.Utils;
+
+using FluentValidation;
 
 using Microsoft.AspNetCore.Http;
 
@@ -6,33 +8,19 @@ namespace EduRankCR.Application.Commands.Profile.Commands.Avatar.Change;
 
 public class ChangeAvatarProfileCommandValidator : AbstractValidator<ChangeAvatarProfileCommand>
 {
-    private static readonly HashSet<string> AllowedMimeTypes = new()
-    {
-        "image/jpeg",
-        "image/png",
-        "image/gif",
-        "image/webp",
-    };
-
     public ChangeAvatarProfileCommandValidator()
     {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required.")
+            .Must(id => Guid.TryParse(id, out _)).WithMessage("Invalid UserId format.");
+
         RuleFor(x => x.Avatar)
-            .Cascade(CascadeMode.Stop)
-            .NotNull()
-            .Must(x => x.Length > 0)
-            .Must(x => x.Length < 1_048_576)
-            .Must(IsValidMimeType);
+            .NotNull().WithMessage("Avatar file is required.")
+            .Must(BeValidAvatar).WithMessage("Invalid file format. Only JPG, JPEG, PNG, or WEBP, GIF files are allowed and the file size must be less than 5MB.");
     }
 
-    private bool IsValidMimeType(IFormFile? file)
+    private static bool BeValidAvatar(IFormFile file)
     {
-        try
-        {
-            return file != null && AllowedMimeTypes.Contains(file.ContentType.ToLower());
-        }
-        catch
-        {
-            return false;
-        }
+        return ValidationHelper.IsValidAvatar(file);
     }
 }
