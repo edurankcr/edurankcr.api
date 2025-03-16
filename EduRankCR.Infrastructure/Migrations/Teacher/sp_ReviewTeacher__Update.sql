491866729fb2b2568aae92c50b2e4964ad056b58
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-16 11:09:05 -0600

    feat: add stored procedure sp_ReviewTeacher__Update for updating teacher reviews

---
+++ EduRankCR.Infrastructure/Migrations/Teacher/sp_ReviewTeacher__Update.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE sp_ReviewTeacher__Update
+    @ReviewId   UNIQUEIDENTIFIER,
+    @FreeCourse BIT,
+    @CourseCode NVARCHAR(64) = NULL,
+    @CourseMode TINYINT,
+    @ProfessorRating DECIMAL(2,1),
+    @DifficultyRating DECIMAL(2,1),
+    @WouldTakeAgain BIT = NULL,
+    @MandatoryAttendance BIT = NULL,
+    @GradeReceived NVARCHAR(5) = NULL,
+    @ExperienceText NVARCHAR(510),
+    @Status TINYINT = 0
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        UPDATE TeachersReviews
+        SET
+            FreeCourse          = COALESCE(@FreeCourse, FreeCourse),
+            CourseCode          = COALESCE(@CourseCode, CourseCode),
+            CourseMode          = COALESCE(@CourseMode, CourseMode),
+            ProfessorRating     = COALESCE(@ProfessorRating, ProfessorRating),
+            DifficultyRating    = COALESCE(@DifficultyRating, DifficultyRating),
+            WouldTakeAgain      = COALESCE(@WouldTakeAgain, WouldTakeAgain),
+            MandatoryAttendance = COALESCE(@MandatoryAttendance, MandatoryAttendance),
+            GradeReceived       = COALESCE(@GradeReceived, GradeReceived),
+            ExperienceText      = COALESCE(@ExperienceText, ExperienceText),
+            Status              = COALESCE(@Status, Status),
+            UpdatedAt           = SYSUTCDATETIME()
+        WHERE ReviewId = @ReviewId;
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
+END;
