46aaecada46e6d255169293395c4411f96360a64
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-26 11:02:36 -0600

    feat: add CORS policy to allow requests from specified origins

--- EduRankCR.Api/Program.cs
+++ EduRankCR.Api/Program.cs
@@ -8,10 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
         .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.WithOrigins("http://localhost:3000", "https://edurankcr.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
 }
 
 var app = builder.Build();
 {
+    app.UseCors();
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.UseAuthentication();
