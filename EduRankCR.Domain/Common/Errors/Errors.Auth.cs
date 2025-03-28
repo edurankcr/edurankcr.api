a4d4f4d7e595b4489320c04bcd1a44dc0d75d204
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-28 09:59:23 -0600

    refactor: update error definitions for InvalidCredentials and Unauthorized in Auth class

--- EduRankCR.Domain/Common/Errors/Errors.Auth.cs
+++ EduRankCR.Domain/Common/Errors/Errors.Auth.cs
@@ -6,7 +6,12 @@ public static partial class Errors
 {
     public static class Auth
     {
-        public static Error InvalidCredentials => Error.Validation(code: "Auth.InvalidCred", description: "Invalid credentials.");
-        public static Error Unauthorized => Error.Validation(code: "Auth.Unauthorized", description: "Login to access this resource.");
+        public static Error InvalidCredentials => Error.Unauthorized(
+            code: "Auth.InvalidCred",
+            description: "Invalid credentials.");
+
+        public static Error Unauthorized => Error.Unauthorized(
+            code: "Auth.Unauthorized",
+            description: "Login to access this resource.");
     }
 }
