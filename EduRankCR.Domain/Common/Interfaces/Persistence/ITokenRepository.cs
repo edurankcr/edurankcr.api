1a24b867a715981bfd6bfdd8b17e62170d3dcfad
Gabriel Marín <amaringonz.dev@gmail.com>
2025-03-28 23:15:04 -0600

    refactor: add FindByUserId method to ITokenRepository for user-specific token retrieval

--- EduRankCR.Domain/Common/Interfaces/Persistence/ITokenRepository.cs
+++ EduRankCR.Domain/Common/Interfaces/Persistence/ITokenRepository.cs
@@ -1,5 +1,6 @@
 ﻿using EduRankCR.Domain.TokenAggregate.Entities;
 using EduRankCR.Domain.TokenAggregate.ValueObjects;
+using EduRankCR.Domain.UserAggregate.ValueObjects;
 
 namespace EduRankCR.Domain.Common.Interfaces.Persistence;
 
@@ -7,6 +8,7 @@ public interface ITokenRepository
 {
     Task Create(Token token);
     Task<Token?> Find(TokenId tokenId);
+    Task<Token?> FindByUserId(UserId userId);
     Task VerifyEmail(TokenId tokenId);
     Task VerifyChangeEmail(TokenId tokenId);
     Task VerifyPassword(TokenId tokenId, string password);
