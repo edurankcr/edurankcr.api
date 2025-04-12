48a655a535ba1468885af8355c7377f0217fdff1
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-12 02:25:52 -0600

    feat: add LastReviewsResponse record for encapsulating last reviews response data

---
+++ EduRankCR.Contracts/Reviews/LastReviewsResponse.cs
@@ -0,0 +1,11 @@
+﻿using EduRankCR.Contracts.Common;
+
+namespace EduRankCR.Contracts.Reviews;
+
+public record LastReviewsResponse(
+    MetaDto Meta,
+    LastReviewsContentDto Result);
+
+public record LastReviewsContentDto(
+    List<ReviewTeacherDto> TeacherReviews,
+    List<ReviewInstituteDto> InstituteReviews);
