7ceee21b7c52f65a8e52dcb352a60d3a772e7c48
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-12 02:39:35 -0600

    feat: remove InstituteId and related fields from sp_Search__All procedure

--- EduRankCR.Infrastructure/Migrations/Search/sp_Search__All.sql
+++ EduRankCR.Infrastructure/Migrations/Search/sp_Search__All.sql
@@ -1,7 +1,6 @@
 ﻿CREATE PROCEDURE sp_Search__All
     @Name NVARCHAR(100),
     @Type NVARCHAR(20) = NULL,
-    @InstituteId UNIQUEIDENTIFIER = NULL,
     @TypeFilter TINYINT = NULL,
     @Province TINYINT = NULL
 AS
@@ -11,16 +10,10 @@ BEGIN
     IF @Type IS NULL OR @Type IN ('teacher', 'all')
         BEGIN
             SELECT
-                'teacher' AS ResultType,
                 t.TeacherId,
                 t.Name,
                 t.LastName,
                 (t.Name + ' ' + t.LastName) AS FullName,
-                NULL AS InstituteId,
-                NULL AS InstituteName,
-                NULL AS Type,
-                NULL AS Province,
-                NULL AS Url,
                 t.CreatedAt,
                 t.UpdatedAt
             FROM Teachers t
@@ -32,13 +25,8 @@ BEGIN
     IF @Type IS NULL OR @Type IN ('institute', 'all')
         BEGIN
             SELECT
-                'institute' AS ResultType,
-                NULL AS TeacherId,
-                NULL AS Name,
-                NULL AS LastName,
-                NULL AS FullName,
                 i.InstituteId,
-                i.Name AS InstituteName,
+                i.Name,
                 i.Type,
                 i.Province,
                 i.Url,
