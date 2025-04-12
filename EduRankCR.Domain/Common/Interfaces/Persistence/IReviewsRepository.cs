33ecd5f68e2486e3642f9e505a6efcee2bf3dcb3
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-12 02:26:16 -0600

    feat: add IReviewsRepository interface for retrieving last reviews

---
+++ EduRankCR.Domain/Common/Interfaces/Persistence/IReviewsRepository.cs
@@ -0,0 +1,8 @@
+﻿using EduRankCR.Domain.Common.Projections;
+
+namespace EduRankCR.Domain.Common.Interfaces.Persistence;
+
+public interface IReviewsRepository
+{
+    Task<(List<ReviewTeacherProjection> TeacherReviews, List<ReviewInstituteProjection> InstituteReviews)> GetLastReviews();
+}
