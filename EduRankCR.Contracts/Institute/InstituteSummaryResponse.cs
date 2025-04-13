58fa215421f8d6024e65393ef1415542e5a30fbb
Gabriel Marín <amaringonz.dev@gmail.com>
2025-04-12 19:51:27 -0600

    feat: add InstituteSummaryResponse record to encapsulate institute summary details

---
+++ EduRankCR.Contracts/Institute/InstituteSummaryResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace EduRankCR.Contracts.Institute;
+
+public record InstituteSummaryResponse(
+    Guid InstituteId,
+    int TotalReviews,
+    decimal TotalAverageScore,
+    decimal Reputation,
+    decimal Opportunities,
+    decimal Happiness,
+    decimal Location,
+    decimal Facilities,
+    decimal Social,
+    decimal Clubs,
+    decimal Internet,
+    decimal Security,
+    decimal Food,
+    DateTime UpdatedAt);
